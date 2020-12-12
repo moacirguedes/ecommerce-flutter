e718c684fd4213bfda814f0d68fd4f3de33f7569
Moacir <guedesmoacir12@gmail.com>
2020-12-12 19:26:22 -0300

    first commit

---
+++ lib/utils/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static final String HOME = "/home";
+  static final String LOGIN = "/login";
+  static final String REGISTER = "/register";
+  static final String PRODUCT = "/product";
+  static final String CART = "/cart";
+  static final String ADDRESS = "/address";
+  static final String CHECKOUT = "/checkout";
+}
---
+++ lib/widgets/cart/cart_body.dart
@@ -0,0 +1,64 @@
+import 'package:ecommerce/providers/cart_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../utils/routes.dart';
+import 'cart_content.dart';
+
+class CartBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CartProvider _cartProvider = Provider.of<CartProvider>(context);
+
+    if (_cartProvider.items.isEmpty) {
+      return Center(
+        child: Text(
+          "O carrinho está vazio",
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.grey[500],
+          ),
+        ),
+      );
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          /**
+           *  Preço Total
+           */
+          CartContent(),
+          const SizedBox(
+            height: 30,
+          ),
+          /**
+           *  Botão de Endereço
+           */
+          RaisedButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(Routes.ADDRESS);
+            },
+            elevation: 0,
+            textColor: Colors.white,
+            color: Colors.lightBlueAccent,
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 15,
+              ),
+              child: Text(
+                "CONTINUAR PARA ENDEREÇO",
+                softWrap: false,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.clip,
+                maxLines: 2,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
